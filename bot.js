2155dac9c0f9d0424ea4a9392e1cdb2536804d1a
HamzaCharko <48889435+HamzaCharko@users.noreply.github.com>
2019-04-11 13:29:20 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -5,7 +5,7 @@ client.on('ready', () => {
    console.log(`----------------`);
       console.log(`Desert Bot- Script By : Hamza`);
         console.log(`----------------`);
-      console.log(`ON ${client.guilds.size} Servers '     Script By : Hamza ' `);
+
     console.log(`----------------`);
   console.log(`Logged in as ${client.user.tag}!`);
 client.user.setGame(`انا بوت`,"http://twitch.tv/S-F")
