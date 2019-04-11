b41a38a6e69ffdb2afc7c11ffb54c336f9c530f4
HamzaCharko <48889435+HamzaCharko@users.noreply.github.com>
2019-04-11 14:40:10 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -8,10 +8,36 @@ client.on('ready', () => {
 
     console.log(`----------------`);
   console.log(`Logged in as ${client.user.tag}!`);
-client.user.setGame(`انا بوت`,"http://twitch.tv/S-F")
-client.user.setStatus("dnd")
-});
+   
+const prefix = "-"
 
+client.on('message', message => {
+  if (!message.content.startsWith(prefix)) return;
+  var args = message.content.split(' ').slice(1);
+  var argresult = args.join(' ');
+  if (message.author.id !== '523159342095269898') return;
+
+if (message.content.startsWith(prefix + 'sp')) {
+  client.user.setGame(argresult);
+    message.channel.sendMessage(`**:white_check_mark:  : ${argresult}**`)
+} else 
+
+if (message.content.startsWith(prefix + 'sw')) {
+client.user.setActivity(argresult, {type:'WATCHING'});
+    message.channel.sendMessage(`**:white_check_mark:  : ${argresult}**`)
+} else 
+if (message.content.startsWith(prefix + 'sl')) {
+client.user.setActivity(argresult, {type:'LISTENING'});
+    message.channel.sendMessage(`**:white_check_mark:  : ${argresult}**`)
+} else 
+
+if (message.content.startsWith(prefix + 'ss')) {
+  client.user.setGame(argresult, "https://www.twitch.tv/Justin-Ly0001");
+    message.channel.sendMessage(`**:white_check_mark:  : ${argresult}**`)
+}
+
+});
+ 
 
 
 client.login(process.env.BOT_TOKEN);
