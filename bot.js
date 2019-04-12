88d22cfee0fb0c8c9a7c4ef881849b8798aa8780
HamzaCharko <48889435+HamzaCharko@users.noreply.github.com>
2019-04-12 03:41:51 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,33 +1,31 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-const prefix = "-"
 
+var adminprefix = "=";
+const devs = ['523159342095269898'];
 client.on('message', message => {
-  if (!message.content.startsWith(prefix)) return;
-  var args = message.content.split(' ').slice(1);
-  var argresult = args.join(' ');
-  if (message.author.id !== '523159342095269898') return;
-
-if (message.content.startsWith(prefix + 'sp')) {
+  var argresult = message.content.split(` `).slice(1).join(' ');
+    if (!devs.includes(message.author.id)) return;
+    
+if (message.content.startsWith(adminprefix + 'setgame')) {
   client.user.setGame(argresult);
-    message.channel.sendMessage(`**:white_check_mark:  : ${argresult}**`)
+    message.channel.sendMessage(`**${argresult} تم تغيير البلاينق إلى **`)
 } else 
-
-if (message.content.startsWith(prefix + 'sw')) {
-client.user.setActivity(argresult, {type:'WATCHING'});
-    message.channel.sendMessage(`**:white_check_mark:  : ${argresult}**`)
-} else 
-if (message.content.startsWith(prefix + 'sl')) {
-client.user.setActivity(argresult, {type:'LISTENING'});
-    message.channel.sendMessage(`**:white_check_mark:  : ${argresult}**`)
-} else 
-
-if (message.content.startsWith(prefix + 'ss')) {
-  client.user.setGame(argresult, "https://www.twitch.tv/Justin-Ly0001");
-    message.channel.sendMessage(`**:white_check_mark:  : ${argresult}**`)
+  if (message.content.startsWith(adminprefix + 'setname')) {
+client.user.setUsername(argresult).then
+    message.channel.sendMessage(`**${argresult}** : تم تغيير أسمك إلى`)
+return message.reply("**لا يمكنك تغيير الاسم يجب عليك الانتظآر لمدة ساعتين . **");
+} else
+  if (message.content.startsWith(adminprefix + 'setavatar')) {
+client.user.setAvatar(argresult);
+  message.channel.sendMessage(`**${argresult}** : تم تغير صورة `);
+      } else     
+if (message.content.startsWith(adminprefix + 'setT')) {
+  client.user.setGame(argresult, "https://www.twitch.tv/idk");
+    message.channel.sendMessage(`**تم تغيير تويتش إلى  ${argresult}**`)
 }
-
 });
+
  
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
